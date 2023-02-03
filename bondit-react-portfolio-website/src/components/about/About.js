6ecd12dc71a2e0abb65474d978f3cd3fde65d00c
Bondit Sinlee <bonditsinlee@gmail.com>
2023-02-03 00:31:32 -0600

    create about me element

--- bondit-react-portfolio-website/src/components/about/About.js
+++ bondit-react-portfolio-website/src/components/about/About.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import './about.css'
+import Resume from '../../assests/bonditCV.pdf'
+import {BsDownload} from 'react-icons/bs'
 
 const About = () => {
   return (
-    <div className="about">
-      <h2>About Me</h2>
-      <p>
-        Hi, my name is Bondit Sinlee and I'm a frontend React.js developer.
-      </p>
-      <a href=''></a>
+    <div className="container about-container">
+      <div className='about-content'>
+        <h2>About Me</h2>
+          <p>
+            Hi, my name is Bondit Sinlee
+          </p>
+          <p>
+            I'm a frontend React.js developer.
+          </p>
+          <a href={Resume} className='btn-primary'>My Resume<BsDownload/></a>
+      </div>
     </div>
   )
 }
