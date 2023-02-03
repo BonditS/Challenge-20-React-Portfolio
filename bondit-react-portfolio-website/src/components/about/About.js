e0ac4ddf9a35a2c9440351d7c448bb246b032c5d
Bondit Sinlee <bonditsinlee@gmail.com>
2023-02-02 19:26:39 -0600

    about me

--- bondit-react-portfolio-website/src/components/about/About.js
+++ bondit-react-portfolio-website/src/components/about/About.js
@@ -3,7 +3,13 @@ import './about.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="about">
+      <h2>About Me</h2>
+      <p>
+        Hi, my name is Bondit Sinlee and I'm a frontend React.js developer.
+      </p>
+      <a href=''></a>
+    </div>
   )
 }
 
